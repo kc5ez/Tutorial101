95fb758ac312c6fe2e7d9724390f0965a61d119f
Ke Cheng <kichingxx@yahoo.com>
2014-01-31 17:04:04 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+var port = 8000;
+app.listen(port);
+console.log("Express app listening on port " + port);
+
+app.get('/', function(req, res){
+    res.send('ayooooooo beautiful. Welcome to CS1501');
+});
