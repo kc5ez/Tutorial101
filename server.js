757b8f10a9662019c70f9bc07a2e10e04b49cd1a
Ke Cheng <kichingxx@yahoo.com>
2014-02-10 17:42:56 -0500

    initial project

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express")
+, path = require('path');
+var app = express();
+
+
+require('./routes')(app);
+app.use(express.static(path.normalize(__dirname) + '/public'))
+app.use(express.errorHandler());
+app.configure(function(){
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());	
+})
+
+app.set('views', path.normalize(__dirname) + '/public');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+var port = Number(process.env.PORT || 8000);
+app.listen(8000);
+console.log("App listening on port: " + port);
